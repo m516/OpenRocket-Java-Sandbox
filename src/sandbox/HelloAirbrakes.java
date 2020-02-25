7222cf42b70c4a69762c0447882cfb80854f52cb
Micah <micah112358@gmail.com>
2020-02-25 16:08:54 -0600

    Create HelloAirbrakes.java

---
+++ src/sandbox/HelloAirbrakes.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package sandbox;
+
+
+
+/**
+ * Create a TCP connection to the airbrake code
+ * @author Micah Mundy
+ *
+ */
+public class HelloAirbrakes {
+	
+	/**
+	 *	Creates a new HelloAirbrakes() instance
+	 */
+	public HelloAirbrakes() {
+		System.out.println("Hello there!");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HelloAirbrakes helloAirbrakes = new HelloAirbrakes();
+	}
+	
+
+}
