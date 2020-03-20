b4ec6dfb8936ac21b7889341aa9c66b9de262bac
Micah <micah112358@gmail.com>
2020-03-20 18:34:35 -0500

    Create Force, Vector

---
+++ src/util/Force.java
@@ -0,0 +1,313 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author Micah
+ *
+ */
+public class Force extends Vector3f {
+	
+	/** A flag that indicates that a certain Force instance is in Newtons **/
+	public static final int UNIT_NEWTON = 0;
+	/** A flag that indicates that a certain Force instance is in kilograms 
+	 * as perceived from sea-level **/
+	public static final int UNIT_KILOGRAM = 1;
+	/** A flag that indicates that a certain Force instance is in pound-feet **/
+	public static final int UNIT_POUND = 2;
+	/** A flag that indicates that a certain Force instance is in ounce-inches **/
+	public static final int UNIT_OUNCE = 3;
+	/** A flag that indicates that a certain Force instance is in Newtons **/
+	public static final int UNIT_DYNE = 4;
+	/** A flag that indicates that a certain Force instance is in Newtons **/
+	public static final int UNIT_GRAM = 5;
+	/** A flag that indicates that a certain Force instance is in Newtons **/
+	public static final int UNIT_KILONEWTON = 6;
+	/** A flag that indicates that a certain Force instance is in Newtons **/
+	public static final int UNIT_KIP = 7;
+	/** A flag that indicates that a certain Force instance is in Newtons **/
+	public static final int UNIT_TON = 8;
+	
+
+	/**
+	 * A conversion table of all units relative to Newtons
+	 */
+	private static final float[] CONVERSION_TABLE = 
+		{
+				1.f,
+				0.1019716f,
+				0.2248089f,
+				3.596943f,
+				100000f,
+				101.9716f,
+				0.001f,
+				0.0002248089f,
+				0.0001019716f
+		};
+	
+	private int unit = 0;
+
+	/**
+	 * Creates a new force
+	 */
+	public Force() {
+		super();
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given by the vector.
+	 * Units are assumed to be Newtons.<br>
+	 * 
+	 * To not use kilograms, add an additional parameter
+	 * at the end of this function.
+	 * @param vector the value of the force
+	 */
+	public Force(Vector3f vector) {
+		super(vector);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are assumed to be Newtons.<br>
+	 * 
+	 * To not use kilograms, add an additional parameter
+	 * at the end of this function.
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public Force(double x, double y, double z) {
+		super(x, y, z);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are assumed to be Newtons.<br>
+	 * 
+	 * To not use kilograms, add an additional parameter
+	 * at the end of this function.
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public Force(float x, float y, float z) {
+		super(x, y, z);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are assumed to be Newtons.<br>
+	 * 
+	 * To not use kilograms, add an additional parameter
+	 * at the end of this function.
+	 * @param x the x-component of this force
+	 * @param y the y-component of this force
+	 * @param z the z-component of this force
+	 */
+	public Force(int x, int y, int z) {
+		super(x, y, z);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are assumed to be Newtons.<br>
+	 * 
+	 * To not use kilograms, add an additional parameter
+	 * at the end of this function.
+	 * @param x the x-component of this force
+	 * @param y the y-component of this force
+	 * @param z the z-component of this force
+	 */
+	public Force(byte x, byte y, byte z) {
+		super(x, y, z);
+	}
+	
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given by the vector.
+	 * Units are specified manually using 
+	 * the <b>units</b> constants contained
+	 * by this class.<br>
+	 * 
+	 * @param vector the value of the force
+	 * 
+	 * @see 	UNIT_KILOGRAM
+	 * @see 	UNIT_POUND
+	 * @see 	UNIT_OUNCE
+	 * @see 	UNIT_DYNE
+	 * @see 	UNIT_GRAM
+	 * @see 	UNIT_KILONEWTON
+	 * @see 	UNIT_KIP
+	 * @see 	UNIT_TON
+	 */
+	public Force(Vector3f vector, int units) {
+		super(vector);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are specified manually using 
+	 * the <b>units</b> constants contained
+	 * by this class.<br>
+	 * 
+	 * @param x the x-component of this force
+	 * @param y the y-component of this force
+	 * @param z the z-component of this force
+	 * 
+	 * @see 	UNIT_KILOGRAM
+	 * @see 	UNIT_POUND
+	 * @see 	UNIT_OUNCE
+	 * @see 	UNIT_DYNE
+	 * @see 	UNIT_GRAM
+	 * @see 	UNIT_KILONEWTON
+	 * @see 	UNIT_KIP
+	 * @see 	UNIT_TON
+	 */
+	public Force(double x, double y, double z, int units) {
+		super(x, y, z);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are specified manually using 
+	 * the <b>units</b> constants contained
+	 * by this class.<br>
+	 * 
+	 * @param x the x-component of this force
+	 * @param y the y-component of this force
+	 * @param z the z-component of this force
+	 * 
+	 * @see 	UNIT_KILOGRAM
+	 * @see 	UNIT_POUND
+	 * @see 	UNIT_OUNCE
+	 * @see 	UNIT_DYNE
+	 * @see 	UNIT_GRAM
+	 * @see 	UNIT_KILONEWTON
+	 * @see 	UNIT_KIP
+	 * @see 	UNIT_TON
+	 */
+	public Force(float x, float y, float z, int units) {
+		super(x, y, z);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are specified manually using 
+	 * the <b>units</b> constants contained
+	 * by this class.<br>
+	 * 
+	 * @param x the x-component of this force
+	 * @param y the y-component of this force
+	 * @param z the z-component of this force
+	 * 
+	 * @see 	UNIT_KILOGRAM
+	 * @see 	UNIT_POUND
+	 * @see 	UNIT_OUNCE
+	 * @see 	UNIT_DYNE
+	 * @see 	UNIT_GRAM
+	 * @see 	UNIT_KILONEWTON
+	 * @see 	UNIT_KIP
+	 * @see 	UNIT_TON
+	 */
+	public Force(int x, int y, int z, int units) {
+		super(x, y, z);
+	}
+
+	/**
+	 * Creates a Force whose x, y, and z 
+	 * components are given.
+	 * Units are specified manually using 
+	 * the <b>units</b> constants contained
+	 * by this class.<br>
+	 * 
+	 * @param x the x-component of this force
+	 * @param y the y-component of this force
+	 * @param z the z-component of this force
+	 * 
+	 * @see 	UNIT_KILOGRAM
+	 * @see 	UNIT_POUND
+	 * @see 	UNIT_OUNCE
+	 * @see 	UNIT_DYNE
+	 * @see 	UNIT_GRAM
+	 * @see 	UNIT_KILONEWTON
+	 * @see 	UNIT_KIP
+	 * @see 	UNIT_TON
+	 */
+	public Force(byte x, byte y, byte z, int units) {
+		super(x, y, z);
+	}
+
+	/**
+	 * @return the units of this Force
+	 */
+	public int getUnit() {
+		return unit;
+	}
+
+	/**
+	 * @param newUnit the unit to set to. Values are adjusted 
+	 * accordingly so that minimal data is lost.
+	 */
+	public void setUnit(int newUnit) {
+		mult(CONVERSION_TABLE[newUnit]/CONVERSION_TABLE[unit]);
+		unit = newUnit;
+	}
+	
+	
+	@Override public Object clone() {
+		Force f = new Force();
+		f.x = x;
+		f.y = y;
+		f.z = z;
+		f.unit = unit;
+		return f;
+	}
+	
+
+	/**
+	 * @return a copy of this Force
+	 */
+	public Force copy() {
+		Force f = new Force();
+		f.x = x;
+		f.y = y;
+		f.z = z;
+		f.unit = unit;
+		return f;
+	}
+	
+	/**
+	 * Adds a force to this one, taking units into account
+	 * @param other the force to add to this one
+	 * 
+	 * @see add
+	 */
+	public void add(Force other) {
+		setUnit(other.unit);
+		translate(other);
+	}
+
+	
+	/**
+	 * Adds a force to this one, taking units into account
+	 * @param other the force to add to this one
+	 * 
+	 * @return the result of the addition of force1 and force2
+	 * 
+	 * @see add
+	 */
+	public static Force add(Force force1, Force force2) {
+		Force newForce = force1.copy();
+		newForce.add(force2);
+		return newForce;
+	}
+}
---
+++ src/util/Vector3f.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author Micah
+ *
+ */
+public class Vector3f {
+
+	
+	public float x;
+	public float y;
+	public float z;
+	
+	/**
+	 * Creates a new vector with x, y, z = 0
+	 */
+	public Vector3f() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	
+	/**
+	 * Creates a new vector with the same coordinates as another vector
+	 * @param vecToCopy the vector to clone
+	 */
+	public Vector3f(Vector3f vecToCopy) {
+		x = vecToCopy.x;
+		y = vecToCopy.y;
+		z = vecToCopy.z;
+	}
+	
+	/**
+	 * Creates a new vector with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 * @param z the z-value of the new vector
+	 */
+	public Vector3f(double x, double y, double z) {
+		this.x = (float)x;
+		this.y = (float)y;
+		this.z = (float)z;
+	}
+
+	/**
+	 * Creates a new vector with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 * @param z the z-value of the new vector
+	 */
+	public Vector3f(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * Creates a new vector with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 * @param z the z-value of the new vector
+	 */
+	public Vector3f(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * Creates a new vector with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 * @param z the z-value of the new vector
+	 */
+	public Vector3f(byte x, byte y, byte z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * Creates a new vector in the XY plane with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 */
+	public Vector3f(double x, double y) {
+		this.x = (float)x;
+		this.y = (float)y;
+	}
+
+	/**
+	 * Creates a new vector in the XY plane with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 */
+	public Vector3f(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Creates a new vector in the XY plane with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 */
+	public Vector3f(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Creates a new vector in the XY plane with certain values
+	 * @param x the x-value of the new vector
+	 * @param y the y-value of the new vector
+	 */
+	public Vector3f(byte x, byte y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Multiply this vector by a scalar. <br>
+	 * 
+	 * If it is necessary to create a new vector that
+	 * is a scalar multiple of this vector, call the
+	 * static method instead. <br>
+	 * 
+	 * <code>
+	 * @param value
+	 */
+	public void mult(float value) {
+		x*=value;
+		y*=value;
+		z*=value;
+	}
+	
+	/**
+	 * Creates a new vector that is a scalar multiple 
+	 * of another vector.
+	 * @param source the source vector
+	 * @param value the scalar value to multiply to the source vector
+	 * @return a new vector that is the result of the multiplication
+	 */
+	public static Vector3f mult(Vector3f source, float value) {
+		Vector3f ret = new Vector3f(source);
+		ret.mult(value);
+		return ret;
+	}
+	
+	/**
+	 * Translates this vector by an offset of another vector
+	 * @param other the vector to offset this one by
+	 */
+	public void translate(Vector3f other) {
+		x+=other.x;
+		y+=other.y;
+		z+=other.z;
+	}
+	
+	/**
+	 * Creates a new vector that is translated by another vector
+	 * @param source a vector
+	 * @param other a vector
+	 * @return the source translated by the other vector
+	 */
+	public static Vector3f translate(Vector3f source, Vector3f other) {
+		Vector3f ret = new Vector3f(source);
+		ret.translate(other);
+		return ret;
+	}
+	
+	
+}
