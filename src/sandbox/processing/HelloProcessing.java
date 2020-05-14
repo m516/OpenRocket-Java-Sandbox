dced7253d0628d826f4ae38de1ba15a16ab859de
Micah <micah112358@gmail.com>
2020-05-14 10:19:39 -0500

    Fix HelloProcessing.java for the applet to work
    A new Processing window can be created with the HelloProcessing.java file. Here are the fixes that were necessary:
    
    Change public void setup() to public void settings(). Processing doesn't like using size() in setup() without the Processing IDE.
    
    Spefify the full path of the sketch class when running PApplet.main(). That way, the class can be registered by the PApplet class.

--- src/sandbox/processing/HelloProcessing.java
+++ src/sandbox/processing/HelloProcessing.java
@@ -6,21 +6,22 @@ package sandbox.processing;
 import processing.core.PApplet;
 
 /**
- * @author Beta
+ * @author Micah Mundy
  *
  */
 public class HelloProcessing extends PApplet {
 	
     // The argument passed to main must match the class name
     public static void main(String[] args) {
-        PApplet.main("HelloProcessing");
+        PApplet.main("sandbox.processing.HelloProcessing");
     }
 	
 	@Override
-	public void setup() {
+	public void settings() {
 		size(640,480);
 	}
 	
+	@Override
 	public void draw() {
 		background(0);
 		fill(255,255,255);
